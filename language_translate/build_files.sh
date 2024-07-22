ed0b87988c723617a0d6c3cd295cd7456d09264c
AkashR0712 <122616990+AkashR0712@users.noreply.github.com>
2024-07-22 18:28:52 +0530

    Update build_files.sh

--- language_translate/build_files.sh
+++ language_translate/build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate
